623cc3bf1873934b544fa01a6f5e00f1433b4489
maddybvh <maddybvh@gmail.com>
2020-03-09 20:59:32 -0400

    JS logic clean up

--- script.js
+++ script.js
@@ -15,49 +15,61 @@ function palindromeArray(numLow, numHigh){
         i+=.01;
         let n = i.toFixed(2);
         if (palindrome(n.toString())){
-            palindromeArray.push(n);            
+            palindromeArray.push(parseFloat(n));         
         }
-    } while (i <= numHigh); 
+    } while (i <= numHigh);
     return palindromeArray
 }
 
 var billAmount = document.getElementById("bill").value;
 var tipPercentLow = document.getElementById("tipPercentLow").value;
 var tipPercentHigh = document.getElementById("tipPercentHigh").value;
-var tipArray = [];
-var totalArray = [];
 
 
-
+//Outputs all possible palindromic tips within the given parameters.
 function findPalTips (billAmount, tipPercentLow, tipPercentHigh){
     tipArray = palindromeArray(billAmount*tipPercentLow*.01, billAmount*tipPercentHigh*.01);
     return tipArray;
 };
 
+//
+function arrayTipsAndTotals(billAmount, tipArray){
+    let i;
+    var allTotalArray = [];
+    for (i of tipArray){
+        let total = Number(i)+ Number(billAmount)
+        let n = total.toFixed(2)
+        allTotalArray.push([i, n]);
+    }
+    return allTotalArray;
+}
+
 //Outputs an array of palindromic tips and their corresponding palendromic totals, if any exist.
 function findPalTotals (billAmount, tipArray){
     let i;
-    var totalArray = [];
+    var palTotalArray = [];
     for (i of tipArray){
         let total = Number(billAmount) + Number(i);
-        console.log(total);
         if (palindrome(total.toString())){
-            totalArray.push([i, total]);
+            palTotalArray.push([i, total]);
         }
     }
-    return totalArray
+    return palTotalArray
 }
 
+
+
+//Event listener, main script calling all other scripts.
 document.getElementById("submitButton").addEventListener('click', (e) => {
     billAmount = document.getElementById("bill").value;
     tipPercentLow = document.getElementById("tipPercentLow").value;
     tipPercentHigh = document.getElementById("tipPercentHigh").value;
 
     tipArray = findPalTips (billAmount, tipPercentLow, tipPercentHigh)
-    console.log(tipArray);
 
-    totalArray = findPalTotals(billAmount, tipArray);
-    console.log(totalArray);
+    palTotalArray = findPalTotals(billAmount, tipArray);
 
-});
+    allTotalArray = arrayTipsAndTotals(billAmount, tipArray);
+    }
+);
 
